3c17c267b6bdad15c2810c62be8c8f913d4c140c
MATTEO MAGRINO <matteo.magrino@istitutofreud.it>
2026-03-24 19:00:19 +0100

    Add Operazioni#Base class and fix some problems

---
+++ Calcolatore di dati statistici/OperazioniBase.h
@@ -0,0 +1,10 @@
+#pragma once
+class OperazioniBase
+{
+	public:
+	float calcoloPotenza(float valore_base, float valore_esponente);
+	float calcoloRadice(float valore_base, float valore_indice);
+	float calcoloLogaritmo(float valore_base, float valore_argomento);
+	float calcoloMedia(float valore1, float valore2);
+};
+
