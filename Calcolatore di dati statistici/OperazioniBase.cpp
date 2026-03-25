c6c35ea585ec97e703c16e87d3751d89dca959a0
MATTEO MAGRINO <matteo.magrino@istitutofreud.it>
2026-03-25 02:15:33 +0100

    Edit class OperazioniBase.cpp

--- Calcolatore di dati statistici/OperazioniBase.cpp
+++ Calcolatore di dati statistici/OperazioniBase.cpp
@@ -1,1 +1,24 @@
 #include "OperazioniBase.h"
+
+OperazioniBase::OperazioniBase()
+{
+	this->lista_risultati_calcoli = {};	
+}
+
+float OperazioniBase::calcoloLogaritmo(float valore_base, float valore_argomento)
+{
+	this->lista_risultati_calcoli.push_back(log(valore_argomento) / log(valore_base));
+    return this->lista_risultati_calcoli.back();
+}
+
+float OperazioniBase::calcoloPotenza(float valore_base, float valore_esponente)
+{
+	for (int i = 0; i < valore_esponente; i++)
+		this->lista_risultati_calcoli.push_back(this->lista_risultati_calcoli.back() * valore_base);
+	return this->lista_risultati_calcoli.back();
+}
+
+float OperazioniBase::calcoloRadice(float valore_base, float valore_indice)
+{
+	//radice = 
+}
--- Calcolatore di dati statistici/OperazioniBase.h
+++ Calcolatore di dati statistici/OperazioniBase.h
@@ -1,10 +1,18 @@
 #pragma once
+
+#include <map>
+#include <list>
+using namespace std;
+
 class OperazioniBase
 {
+	private:
+		list<float> lista_risultati_calcoli;
 	public:
-	float calcoloPotenza(float valore_base, float valore_esponente);
-	float calcoloRadice(float valore_base, float valore_indice);
-	float calcoloLogaritmo(float valore_base, float valore_argomento);
-	float calcoloMedia(float valore1, float valore2);
-};
+		OperazioniBase();
+		float calcoloPotenza(float valore_base, float valore_esponente);
+		float calcoloRadice(float valore_base, float valore_indice);
+		float calcoloLogaritmo(float valore_base, float valore_argomento);
+		float calcoloSommatoria(float array_valori[]);
 
+};
